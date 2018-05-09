4b28da2a92da8c7532cbe2fa1cf80bc4a8cc7e85
ghmsn <ghmsn@126.com>
2018-05-09 18:46:56 +0800

    1.5.2

--- src/main/java/cn/bdc/wcs/WcsApplication.java
+++ src/main/java/cn/bdc/wcs/WcsApplication.java
@@ -3,6 +3,15 @@ package cn.bdc.wcs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author guhm
+ * @version 1.0
+ * 
+ * wcs启动程序
+ * 
+ *
+ */
 @SpringBootApplication
 public class WcsApplication {
 
